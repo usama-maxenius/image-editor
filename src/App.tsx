cc47ed3e51efe7e05e8b9cccae5fd2e8dbdfd9e8
NaveedSahotra555 <naveed.sahotra3@gmail.com>
2023-11-30 13:35:17 +0500

    feat/fabric-setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import LandingPage from "./pages/landing page/landingPage";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import EgBanner from "./pages/egBannar/egBanner";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Templates from "./pages/templates/templates";
+import ImageEditor from "./components/ImageEditor/fabricImageEditor";
 
 function App() {
- 
   return (
     <>
-    
-    <Router>
+      <Router>
         <Routes>
-        <Route path="/" element={<LandingPage/>} />
-        <Route path="/next" element={<EgBanner/>} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/templates" element={<Templates />} />
+          <Route path="/image-editor" element={<ImageEditor />} />
 
         </Routes>
-    </Router>
-    
-   
+      </Router>
     </>
-  ); 
+  );
 }
 
 export default App;
---
+++ src/components/TemplateCard/card.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Card, CardContent, Typography } from "@mui/material";
+import { styled } from "@mui/system";
+import { useNavigate } from "react-router-dom";
+
+const StyledCard = styled(Card)(({ theme }) => ({
+  height: 400,
+  width: 300,
+  backgroundColor: "#1E1E3C",
+  color: "white",
+  backgroundImage: `url('https://png.pngtree.com/thumb_back/fh260/background/20210716/pngtree-orange-background-portrait-simple-modern-banner-image_746302.jpg')`,
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+}));
+
+const StyledCardContent = styled(CardContent)({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+});
+function MainCard() {
+  const navigate = useNavigate();
+
+  return (
+    <StyledCard onClick={()=> navigate("/image-editor")}>
+      <StyledCardContent>
+        <Typography variant="h4" gutterBottom>
+          Welcome to My App
+        </Typography>
+      </StyledCardContent>
+    </StyledCard>
+  );
+}
+
+export default MainCard;
