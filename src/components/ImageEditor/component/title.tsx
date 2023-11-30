953e615d0c1251044286ea1e6001ec03f94aad76
NaveedSahotra555 <naveed.sahotra3@gmail.com>
2023-11-30 16:10:35 +0500

    feat/text-dynamically changing on canvas

--- src/components/ImageEditor/component/title.tsx
+++ src/components/ImageEditor/component/title.tsx
@@ -1,10 +1,39 @@
-function Title(){
-    return(
-        <>
-        
-        this is Title 
-        </>
-    )
+import { useTitle } from "../../../context/TitleContext";
+
+function Title() {
+  const { setTitle } = useTitle();
+
+  const handleParagraphClick = (content: string) => {
+    setTitle(content);
+  };
+
+  return (
+    <>
+      <h3>Select Title</h3>
+      <p
+        onClick={() =>
+          handleParagraphClick(
+            "SEN. SCOTT TO SPEND $6M ON TV ADS IN IOWA, NH: 2024 GOP PRIMARY"
+          )
+        }
+      >
+        SEN. SCOTT TO SPEND $6M ON TV ADS IN IOWA, NH: 2024 GOP PRIMARY
+      </p>
+      <p
+        onClick={() =>
+          handleParagraphClick("ADIPIS ELIT, SED DO EIUSMOD TEMPOR")
+        }
+      >
+        ADIPIS ELIT, SED DO EIUSMOD TEMPOR
+      </p>
+      <p onClick={() => handleParagraphClick("LOREM IPSUM DOLOR SIT AMET")}>
+        LOREM IPSUM DOLOR SIT AMET,
+      </p>{" "}
+      <p onClick={() => handleParagraphClick("LOREM IPSUM  EIUSMOD TEMPOR")}>
+        LOREM IPSUM EIUSMOD TEMPOR
+      </p>
+    </>
+  );
 }
 
-export default Title
+export default Title;
--- src/components/ImageEditor/fabricImageEditor.tsx
+++ src/components/ImageEditor/fabricImageEditor.tsx
@@ -1,12 +1,13 @@
 import React, { useRef, useEffect, useState } from "react";
 import { fabric } from "fabric";
 import yourJsonFile from "../Templates/first.json"; // Update the path
-import Background from "./component/background";
 import { Link, Outlet } from "react-router-dom";
-
-
+import { useTitle } from "../../context/TitleContext";
 
 const ImageEditor = () => {
+  const { title } = useTitle();
+  console.log("title", title);
+
   const canvasRef: React.MutableRefObject<fabric.Canvas | null> =
     useRef<fabric.Canvas | null>(null);
 
@@ -32,8 +33,9 @@ const ImageEditor = () => {
 
     const loadJsonFile = async (json: any) => {
       try {
-        canvas.loadFromJSON(json, () => {
+        canvas.loadFromJSON(json, (o: any, object: any) => {
           hideLoading();
+
           const imageObject = canvas
             .getObjects()
             .find((object) => object.type === "image");
@@ -41,6 +43,20 @@ const ImageEditor = () => {
             //@ts-ignore
             imageObject.setSrc(imageObject.src);
           }
+          if (title.trim() !== "") {
+            // Find objects with "cs-type": "title" and update them
+            canvas.getObjects().forEach((object) => {
+              //@ts-ignore
+              if (object["cs-type"] === "title") {
+                //@ts-ignore
+                object.set({ text: title });
+              }
+            });
+
+            // Render canvas after updating objects
+            canvas.renderAll();
+          }
+
           canvas.renderAll();
         });
       } catch (error) {
@@ -50,7 +66,7 @@ const ImageEditor = () => {
     };
 
     loadJsonFile(yourJsonFile);
-  }, []);
+  }, [title]);
 
   const handleButtonClick = (message: string) => {
     // Handle button click based on the message
@@ -71,33 +87,61 @@ const ImageEditor = () => {
         }}
       >
         {loading && <div>Loading...</div>}
-        <div style={{display:'flex',columnGap:'30px'}}>
-        <div
-          style={{
-            width: "500px",
-            height: "500px",
-            border: "3px solid yellow",
-          }}
-        >
-          <canvas
-            ref={canvasRef as React.LegacyRef<HTMLCanvasElement>}
-          ></canvas>
-        </div>
+        <div style={{ display: "flex", columnGap: "30px" }}>
+          <div
+            style={{
+              width: "500px",
+              height: "500px",
+              border: "3px solid #9475bf",
+              borderRadius: "3px",
+            }}
+          >
+            <canvas
+              ref={canvasRef as React.LegacyRef<HTMLCanvasElement>}
+            ></canvas>
+          </div>
 
-        <div style={{color:'white', backgroundColor:'red',width:"150px"}}> <Outlet/> </div>
+          <div
+            style={{ color: "white", backgroundColor: "red", width: "150px" }}
+          >
+            {" "}
+            <Outlet />{" "}
+          </div>
         </div>
 
         <div style={{ display: "flex", marginTop: "16px" }}>
-        <Link to='/image-editor/background'> <button onClick={() => handleButtonClick("It's Background")}>Background</button></Link>
-        <Link to='/image-editor/title'> <button onClick={() => handleButtonClick("It's Title")}>Title</button></Link> 
-        <Link to='/image-editor/bubble '> <button onClick={() => handleButtonClick("Add Bubble")}>Add Bubble</button></Link> 
-        <Link to='/image-editor/element'> <button onClick={() => handleButtonClick("Add Bubble")}>Elements</button></Link> 
-        <Link to='/image-editor/write-post'> <button onClick={() => handleButtonClick("Add Bubble")}>write post</button></Link> 
-
+          <Link to="/image-editor/background">
+            {" "}
+            <button onClick={() => handleButtonClick("It's Background")}>
+              Background
+            </button>
+          </Link>
+          <Link to="/image-editor/title">
+            {" "}
+            <button onClick={() => handleButtonClick("It's Title")}>
+              Title
+            </button>
+          </Link>
+          <Link to="/image-editor/bubble ">
+            {" "}
+            <button onClick={() => handleButtonClick("Add Bubble")}>
+              Add Bubble
+            </button>
+          </Link>
+          <Link to="/image-editor/element">
+            {" "}
+            <button onClick={() => handleButtonClick("Add Bubble")}>
+              Elements
+            </button>
+          </Link>
+          <Link to="/image-editor/write-post">
+            {" "}
+            <button onClick={() => handleButtonClick("Add Bubble")}>
+              write post
+            </button>
+          </Link>
         </div>
       </div>
-
-      
     </>
   );
 };
