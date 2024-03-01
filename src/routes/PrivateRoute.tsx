335393c041dd6f41ef8eafbfcb6a462515d7b192
asad195 <81696767+Asad195@users.noreply.github.com>
2024-03-01 11:11:19 +0500

    minor private route fixes

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,7 +1,7 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { Navigate } from 'react-router-dom';
 
-function PrivateRoute({ children }: any) {
+function PrivateRoute({ children }: { children: React.ReactNode }) {
 	const { isAuthenticated } = useAuth0();
 	return isAuthenticated ? children : <Navigate to='/' replace />;
 }
