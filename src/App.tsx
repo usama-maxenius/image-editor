c716b5d915b48c84003a8fd7e648ed434c7a114a
NaveedSahotra555 <naveed.sahotra3@gmail.com>
2024-04-23 17:25:49 +0500

    husky configuration eslint and prettiers

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,38 @@
-import { Route, Routes } from 'react-router-dom';
-import News from './pages/news';
-import HomePage from './pages/homePage';
-import Header from './components/Header';
-import UserInfo from './pages/userInfo';
-import PrivateRoute from './routes/PrivateRoute';
+import { Route, Routes } from "react-router-dom";
+import News from "./pages/news";
+import HomePage from "./pages/homePage";
+import Header from "./components/Header";
+import UserInfo from "./pages/userInfo";
+import PrivateRoute from "./routes/PrivateRoute";
 
 function App() {
-	return (
-		<>
-			<Header />
 
-			<Routes>
-				<Route path='/' element={<HomePage />} />
-				<Route
-					path='/user-info'
-					element={
-						<PrivateRoute>
-							<UserInfo />
-						</PrivateRoute>
-					}
-				/>
-				<Route
-					path='/news'
-					element={
-						<PrivateRoute>
-							<News />
-						</PrivateRoute>
-					}
-				/>
-			</Routes>
-		</>
-	);
+	// testing
+    return (
+        <>
+            <Header />
+
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route
+                    path="/user-info"
+                    element={
+                        <PrivateRoute>
+                            <UserInfo />
+                        </PrivateRoute>
+                    }
+                />
+                <Route
+                    path="/news"
+                    element={
+                        <PrivateRoute>
+                            <News />
+                        </PrivateRoute>
+                    }
+                />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
